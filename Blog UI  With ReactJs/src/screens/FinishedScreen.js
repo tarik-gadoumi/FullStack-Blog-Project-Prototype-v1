10e1da40b907ee16d72821f3666a41442ac62088
tarik-gadoumi <tarik.gadoumi@laplateforme.io>
2022-02-21 08:48:40 +0100

    Refactoring Finished  screen

--- Blog UI  With ReactJs/src/screens/FinishedScreen.js
+++ Blog UI  With ReactJs/src/screens/FinishedScreen.js
@@ -1,34 +1,56 @@
 import * as React from "react";
-import { PostsListUL } from "../components/lib";
+// import { PostsListUL } from "../components/lib";
 import { Link } from "../components/lib";
-import { clientGetUserFinishedPosts } from "../utils/api-client";
-import { PostRow } from "../components/post-row";
-import { useQuery } from "react-query";
+// import { clientGetUserFinishedPosts } from "../utils/api-client";
+// import { PostRow } from "../components/post-row";
+// import { useQuery } from "react-query";
+import { ListItemsList } from "../components/ListItemsReadingAndFinished";
 function FinishedListScreen({ user }) {
-  const { data: FinishedReadingItems } = useQuery({
-    queryKey: "Finished-items",
-    queryFn: () =>
-      clientGetUserFinishedPosts({ token: user.token }).then((data) => {
-        return data.data.data;
-      }),
-  });
+  // const { data: FinishedReadingItems } = useQuery({
+  //   queryKey: "Finished-items",
+  //   queryFn: () =>
+  //     clientGetUserFinishedPosts({ token: user.token }).then((data) => {
+  //       return data.data.data;
+  //     }),
+  // });
 
-  return FinishedReadingItems?.length > 0 ? (
-    <PostsListUL css={{ marginTop: 20 }}>
-      {FinishedReadingItems.map((obj) => {
-        const post = JSON.parse(obj.post);
-        return (
-          <li key={obj.id} aria-label={post.title}>
-            <PostRow key={obj.id} post={post} user={user} />
-          </li>
-        );
-      })}
-    </PostsListUL>
-  ) : (
-    <p>
-      For more ressources go to
-      <Link to="/Discover" children="Discover page" />
-    </p>
+  // return FinishedReadingItems?.length > 0 ? (
+  //   <PostsListUL css={{ marginTop: 20 }}>
+  //     {FinishedReadingItems.map((obj) => {
+  //       const post = JSON.parse(obj.post);
+  //       return (
+  //         <li key={obj.id} aria-label={post.title}>
+  //           <PostRow key={obj.id} post={post} user={user} />
+  //         </li>
+  //       );
+  //     })}
+  //   </PostsListUL>
+  // ) : (
+  //   <p>
+  //     For more ressources go to
+  //     <Link to="/Discover" children="Discover page" />
+  //   </p>
+  // );
+  return (
+    <ListItemsList
+      user={user}
+      callback={(li) => Boolean(li.finishDate)}
+      noListItems={
+        <p>
+          Hey there! This is where posts will go when you've finished reading
+          them. Get started by heading over to{" "}
+          <Link to="/discover">the Discover page</Link> to add Posts to your
+          list.
+        </p>
+      }
+      noFilteredListItems={
+        <p>
+          Looks like you've got some reading to do! Check them out in your{" "}
+          <Link to="/finished">finished books</Link> or{" "}
+          <Link to="/discover">discover more</Link>.
+        </p>
+      }
+    />
   );
 }
 export { FinishedListScreen };
